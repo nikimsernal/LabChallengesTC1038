726bed02373c139f091a0911d1a7f4486aba19b9
Nikita Serna <176351910+nikimsernal@users.noreply.github.com>
2026-08-28 15:57:09 -0600

    Submited bill decomposition solution
    Implement bill decomposition for a given amount.

---
+++ Challenge02/Solution_Challenge2_Bill_Decomposition.py
@@ -0,0 +1,31 @@
+def main():
+    #n = int(input())
+    n = 1865
+    
+    ballenas = n//500
+    n -= ballenas*500
+    
+    sorjuana = n//200
+    n -= sorjuana*200
+    
+    moctezuma = n//100
+    n -= moctezuma*100
+    
+    ajolote = n//50
+    n -= ajolote*50
+    
+    benito = n//20
+    n -= benito*20
+    
+    print("500: ", ballenas)
+    print("200: ", sorjuana)
+    print("100: ", moctezuma)
+    print("50: ", ajolote)
+    print("20: ", benito)
+    
+    if n > 0:
+        print("Remaining: ", n)
+
+main()
+
+
